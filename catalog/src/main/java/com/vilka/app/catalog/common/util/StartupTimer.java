565aacf23085d0254500dbd45f2cf44122f91f13
Christy John <christyjohn.crz@gmail.com>
2026-04-29 19:25:36 +0530

    Added propeties, util file and ran the 1st flyway migration

---
+++ catalog/src/main/java/com/vilka/app/catalog/common/util/StartupTimer.java
@@ -0,0 +1,19 @@
+package com.vilka.app.catalog.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupTimer {
+    private static final Logger log = LoggerFactory.getLogger(StartupTimer.class);
+    private final long startTime = System.currentTimeMillis();
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void logStartupTime() {
+        long time = System.currentTimeMillis() - startTime;
+        log.info("🚀 App started in {} ms", time);
+    }
+}
